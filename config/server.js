40ab07187d25a8a0828b40248655fe11f478059d
Leonardo Monge <lmongea99@gmail.com>
2022-01-07 13:07:09 -0500

    Data Strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ff4ac2ba84f387cc3e8570dd5c1535eb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '883e4352-84cf-4ef0-bbf0-4e7fa8b83c0e'
+};
